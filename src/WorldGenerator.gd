e5bc860862e015f86f464549d6b264efb0c518ba
Lonecat <lonecat.pawstar@gmail.com>
2020-12-02 19:40:38 +0000

    Added road generation to world generator

--- src/WorldGenerator.gd
+++ src/WorldGenerator.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
-onready var tileMapGrass = $TileMap
+onready var tileMap: TileMap = $TileMap
 
 var map_size: Vector2 = Vector2(32, 19)
 
 var grass_cap: float = 0.5
+var road_caps: Vector2 = Vector2(0.3, 0.05)
 
 var noise: OpenSimplexNoise
 
@@ -22,18 +23,32 @@ func _process(delta: float) -> void:
 
 
 func initialize() -> void:
-	tileMapGrass.clear()
+	tileMap.clear()
 	noise = OpenSimplexNoise.new()
 	noise.seed = randi()
 	noise.octaves = 1.0
 	noise.period = 12
+	
 	make_grass_map()
+	make_road_map()
+	tileMap.update_bitmask_region(Vector2(0, 0), Vector2(map_size.x, map_size.y))
+
 
 
 func make_grass_map():
+	var tileId: int = tileMap.get_tileset().find_tile_by_name("grass")
 	for x in map_size.x:
 		for y in map_size.y:
 			var a = noise.get_noise_2d(x, y)
 			if a < grass_cap:
-				tileMapGrass.set_cell(x, y, 0)
-	tileMapGrass.update_bitmask_region(Vector2(0, 0), Vector2(map_size.x, map_size.y))
+				tileMap.set_cell(x, y, tileId)
+
+
+func make_road_map():
+	var tileId: int = tileMap.get_tileset().find_tile_by_name("road")
+	for x in map_size.x:
+		for y in map_size.y:
+			var a = noise.get_noise_2d(x, y)
+			if a < road_caps.x and a > road_caps.y:
+				tileMap.set_cell(x, y, tileId)
+
