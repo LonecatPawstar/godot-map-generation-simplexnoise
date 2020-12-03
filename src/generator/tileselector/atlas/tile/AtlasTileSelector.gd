956e38179c92a70c189a013638a632016d1110aa
Lonecat <lonecat.pawstar@gmail.com>
2020-12-03 17:48:01 +0000

    Refactored atlas tile selector to initialize once only

--- src/generator/tileselector/atlas/tile/AtlasTileSelector.gd
+++ src/generator/tileselector/atlas/tile/AtlasTileSelector.gd
@@ -9,6 +9,12 @@ var _tileMap: TileMap = null
 var _atlasName: String
 
 var _tileSet: TileSet = null
+var _tileId: int = 0 # The atlas tile ID
+
+var _totalColumns: int = 0 # Number of columns in the atlas
+var _totalRows: int = 0 # Number of rows in the atlas
+var _totalPriority: int = 0 # Total of all priorities
+var _arrayPositionPriority: Array = [] # Array of Vector3
 
 
 func initialize(
@@ -17,7 +23,35 @@ func initialize(
 ) -> void:
 	_tileMap = tileMap
 	_atlasName = atlasName
+	
 	_tileSet = _tileMap.get_tileset()
+	_tileId = _tileSet.find_tile_by_name(_atlasName)
+	
+	_initialize_columns_rows()
+	_initialize_position_priority_array()
+
+
+# Counts columns and rows, and stores the count
+func _initialize_columns_rows():
+	# Atlas "region" coordinates in the tileset, e.g. (startx = 256, starty=0, sizex=32, sizey=32)
+	var regionAtlas: Rect2 = _tileSet.tile_get_region(_tileId)
+	# Dimensions x/y of one of the atlas item, e.g. x16/y16
+	var dimensionAtlasItem: Vector2 = _tileSet.autotile_get_size(_tileId)
+	# Number of columns and row in the atlas region
+	_totalColumns = max(regionAtlas.size.x / dimensionAtlasItem.x, 1) # Minimum of 1
+	_totalRows = max(regionAtlas.size.y / dimensionAtlasItem.y, 1) # Minimum of 1
+	
+
+# Iterates over all tiles positions within an atlas, and record the priority of each
+func _initialize_position_priority_array():
+	for c in range(0, _totalColumns):
+		for r in range(0, _totalRows):
+			var priority: int = _tileSet.autotile_get_subtile_priority(
+				_tileId, 
+				Vector2(c, r)
+			)
+			_totalPriority += priority
+			_arrayPositionPriority.append(Vector3(c, r, priority)) # Store prio on Z
 
 
 func _assert() -> void:
@@ -35,48 +69,20 @@ func _assert() -> void:
 # This can be passed to the set_cell method.
 func get_random_tile_from_atlas() -> TileSelected:
 	_assert()
-	var tileIdAtlas: int = _tileSet.find_tile_by_name(_atlasName)
-	# Atlas "region" coordinates in the tileset, e.g. (startx = 256, starty=0, sizex=32, sizey=32)
-	var regionAtlas: Rect2 = _tileSet.tile_get_region(tileIdAtlas)
-	# Dimensions x/y of one of the atlas item, e.g. x16/y16
-	var dimensionAtlasItem: Vector2 = _tileSet.autotile_get_size(tileIdAtlas)
-	# Number of columns and row in the atlas region
-	var numberOfColumns: int = max(regionAtlas.size.x / dimensionAtlasItem.x, 1) # Minimum of 1
-	var numberOfRows: int = max(regionAtlas.size.y / dimensionAtlasItem.y, 1) # Minimum of 1
-	# Random tile selected corresponding to item
 	return AtlasTile.from(
-		tileIdAtlas,
+		_tileId,
 		_atlasName,
-		_get_subtile_with_priority(
-			numberOfColumns,
-			numberOfRows,
-			tileIdAtlas
-		)
+		_get_subtile_with_priority()
 	)
 
 
 # Gets a subtile from the autotile or atlas, using priority
-func _get_subtile_with_priority(
-	numberOfColumns: int,
-	numberOfRows: int,
-	tileIdAtlas: int
-) -> Vector2:
-	var totalPriority = 0
-	var arrayPositionPrio: Array
-	# Iterate over all tiles positions and record them, using Vector2
-	for c in range(0, numberOfColumns):
-		for r in range(0, numberOfRows):
-			var priority: int = _tileSet.autotile_get_subtile_priority(
-				tileIdAtlas, 
-				Vector2(c, r)
-			)
-			totalPriority += priority
-			arrayPositionPrio.append(Vector3(c, r, priority)) # Store prio on Z
+func _get_subtile_with_priority() -> Vector2:
 	# Select one tile based on priority
-	var selectedPriorityId = randi() % totalPriority + 1
-	totalPriority = 0
-	for i in range(0, arrayPositionPrio.size()):
-		var posAndPrio: Vector3 = arrayPositionPrio[i]
+	var selectedPriorityId = randi() % _totalPriority + 1
+	var totalPriority: int = 0
+	for i in range(0, _arrayPositionPriority.size()):
+		var posAndPrio: Vector3 = _arrayPositionPriority[i]
 		totalPriority += posAndPrio.z
 		if (totalPriority >= selectedPriorityId):
 			return Vector2(posAndPrio.x, posAndPrio.y)
