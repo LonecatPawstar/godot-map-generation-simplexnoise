8c89887118c52074631527258f06764fcaf3b23d
Lonecat <lonecat.pawstar@gmail.com>
2020-12-03 19:48:41 +0000

    Cleaned up code and removed multiple initializations

--- src/generator/WorldGenerator.gd
+++ src/generator/WorldGenerator.gd
@@ -8,7 +8,14 @@ const map_size: Vector2 = Vector2(32, 19)
 const grass_cap: float = 0.5
 const road_caps: Vector2 = Vector2(0.3, 0.05)
 
+
 var noise: OpenSimplexNoise
+
+# Tiles
+var tileIdGrass: int
+var tileIdRoad: int
+var tileSelectorAtlases: AtlasesTileSelector
+
 
 func _ready() -> void:
 	#randomize()
@@ -25,16 +32,47 @@ func _process(delta: float) -> void:
 
 func initialize() -> void:
 	tileMap.clear()
-	noise = OpenSimplexNoise.new()
-	noise.seed = randi()
-	noise.octaves = 1.0
-	noise.period = 12
+	
+	_init_noise()
+	_init_tile_selectors()
 	
 	make_map()
 	#make_grass_map()
 	#make_road_map()
 	tileMap.update_bitmask_region(Vector2(0, 0), Vector2(map_size.x, map_size.y))
 	tileMapItems.update_bitmask_region(Vector2(0, 0), Vector2(map_size.x, map_size.y))
+	
+	_free_all()
+
+
+func _init_noise() -> void:
+	noise = OpenSimplexNoise.new()
+	noise.seed = randi()
+	noise.octaves = 1.0
+	noise.period = 12
+
+
+func _init_tile_selectors() -> void:
+	
+	var tileSet: TileSet = tileMap.get_tileset()
+	# Simple tile
+	tileIdGrass = tileSet.find_tile_by_name("grass-solo")
+	# Auto tile
+	tileIdRoad = tileSet.find_tile_by_name("road")
+	
+	# Atlas
+	tileSelectorAtlases = TileSelectorFactory.AtlasesTileSelectorWithPriorityFrom(
+			tileMapItems,
+			[
+				["atlas-rocks", 	5], 
+				["atlas-mushroom", 	4], 
+				["tree", 			1],
+			]
+		)
+
+
+func _free_all() -> void:
+	tileSelectorAtlases.free()
 
 
 ############
@@ -44,32 +82,19 @@ func make_map():
 	for x in map_size.x:
 		for y in map_size.y:
 			var noiseValue: float = noise.get_noise_2d(x, y)
-			select_tile_for_tilemap(x, y, noiseValue)
+			select_tile_for_tilemaps(x, y, noiseValue)
+
 
 # Selects a tile with some rules
-func select_tile_for_tilemap(x: int, y: int, selection: float) -> void:
-	# Tileset for TileMap
-	var tileSet: TileSet = tileMap.get_tileset()
-	var tileIdGrassSolo: int = tileSet.find_tile_by_name("grass-solo")
-	var tileIdRoad: int = tileSet.find_tile_by_name("road")
-	
+func select_tile_for_tilemaps(x: int, y: int, selection: float) -> void:
 	var tileTypeSelected: int = -1
-	var tileItemTypeSelected: int = -1
+	
+	# Grass layer
 	if selection < grass_cap:
-		tileTypeSelected = tileIdGrassSolo
+		tileTypeSelected = tileIdGrass
 		
-		# Select random tile from array of atlases
-		var atlasesTileSelector: AtlasesTileSelector = TileSelectorFactory.AtlasesTileSelectorWithPriorityFrom(
-				tileMapItems,
-				[
-					["atlas-rocks", 	5], 
-					["atlas-mushroom", 	4], 
-					["tree", 			1]
-				]
-			)
-		var tileSelected: TileSelected = atlasesTileSelector.get_random_tile()
-		atlasesTileSelector.free()
-		
+		var tileSelected: TileSelected = tileSelectorAtlases.get_random_tile()
+		# Add items on grass
 		tileMapItems.set_cell(
 				x, y, 
 				tileSelected.tileId, 
@@ -77,8 +102,14 @@ func select_tile_for_tilemap(x: int, y: int, selection: float) -> void:
 				tileSelected.tileSelected
 		)
 	
+	# Road layer
 	if selection < road_caps.x and selection > road_caps.y:
 		tileTypeSelected = tileIdRoad
+		# Erase items on roads
+		tileMapItems.set_cell(
+				x, y, 
+				-1
+		)
 		
 	tileMap.set_cell(x, y, tileTypeSelected)
 
