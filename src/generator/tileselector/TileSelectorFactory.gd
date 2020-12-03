09953ec5beb16c58f8465324fc74ec5f24061943
Lonecat <lonecat.pawstar@gmail.com>
2020-12-03 19:26:47 +0000

    Added validation of atlas selector

--- src/generator/tileselector/TileSelectorFactory.gd
+++ src/generator/tileselector/TileSelectorFactory.gd
@@ -2,6 +2,9 @@ extends Node
 class_name TileSelectorFactory
 
 
+const ERROR_ATLAS_PRIORITY_FORMAT: String = "Invalid atlas and priority array '%s'. Atlas and priority must be passed as an array of arrays [string, int], e.g. [[\"name1\", 2], [\"name2\", 5]]"
+
+
 # Select tiles in atlases.
 # atlasNames example: ["rocks", "mushrooms"]
 static func AtlasesTileSelectorFrom(
@@ -10,9 +13,14 @@ static func AtlasesTileSelectorFrom(
 ) -> AtlasesTileSelector:
 	var arrayAtlasNamesAndPriorities: Array # ["name", 1]
 	for i in range(0, atlasNames.size()):
+		var atlasName = atlasNames[i]
+		assert(
+			atlasName is String, 
+			"Invalid atlas with name '" + str(atlasName) + "'. Atlas names must be passed as an array of Strings, e.g. [\"name1\", \"name2\"]"
+		)
 		arrayAtlasNamesAndPriorities.append(
 			[
-				atlasNames[i],
+				atlasName,
 				1
 			]
 		)
@@ -31,7 +39,22 @@ static func AtlasesTileSelectorWithPriorityFrom(
 	var arrayAtlasSelectorPriority
 	for i in range(0, atlasNamesAndPriorities.size()):
 		arrayAtlasSelectorPriority = []
+		
 		var atlasNameAndPriority: Array = atlasNamesAndPriorities[i]
+		var errorString = ERROR_ATLAS_PRIORITY_FORMAT % str(atlasNameAndPriority)
+		assert(
+			atlasNameAndPriority is Array, 
+			errorString
+		)
+		assert(
+			atlasNameAndPriority[0] is String, 
+			errorString
+		)
+		assert(
+			atlasNameAndPriority[1] is int, 
+			errorString
+		)
+		
 		arrayAtlasSelectorPriority.append(
 			AtlasTileSelectorFactory.from(tileMap, atlasNameAndPriority[0])
 		)
